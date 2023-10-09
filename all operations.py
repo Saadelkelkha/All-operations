b588386e578efac4092c23a435d34e348d857480
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-09 15:08:58 +0100

    Add files via upload

---
+++ all operations.py
@@ -0,0 +1,16 @@
+x=int(input("type the first nomber:"))
+y=int(input("type the second nomber:"))
+somme=x+y
+print("Addition is :",somme)
+diff=x-y
+print("Subtraction is :",diff)
+pro=x*y
+print("Multiplication is :",pro)
+qr=x/y
+print("Division is :",qr)
+qe=x//y
+print("the entire quotient is :",qe)
+mod=x%y
+print("the module is :",mod)
+moy=(x+y)/2
+print("the average is :",moy)
